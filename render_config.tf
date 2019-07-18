eee3f3476c57d0b7786f9ac239f8ed2241c11ba8
kxle6 <kyleatpanw@gmail.com>
2019-07-18 09:58:12 -0700

    Update render_config.tf
    updated file name

--- render_config.tf
+++ render_config.tf
@@ -2,28 +2,28 @@
    to reflect the proper parameters for the IP Address.
 */
 
-data "template_file" "fw1_config_push" {
-  template = "${file("templates/fw1_config_push")}"
+data "template_file" "check_fw_import_gp" {
+  template = "${file("templates/check_fw_import_gp.sh")}"
 
   vars {
     fw1_mgmt_ip = "${aws_eip.ManagementElasticIP.public_ip}"
   }
 }
 
-resource "local_file" "fw1_config_push" {
-  content  = "${data.template_file.fw1_config_push.rendered}"
-  filename = "templates/fw1_config_push.sh"
+resource "local_file" "check_fw_import_gp" {
+  content  = "${data.template_file.check_fw_import_gp.rendered}"
+  filename = "templates/check_fw_import_gp.sh"
 }
 
 /* Check that Firewall 1 is up and push the rendered configs */
-resource "null_resource" "fw1_check_and_push" {
-  depends_on = ["local_file.fw1_config_push"]
+resource "null_resource" "check_fw_import_gp" {
+  depends_on = ["local_file.check_fw_import_gp"]
 
   triggers {
     key = "${aws_instance.FWInstance.id}"
   }
 
   provisioner "local-exec" {
-    command = "templates/fw1_config_push.sh"
+    command = "templates/check_fw_import_gp.sh"
   }
 }
